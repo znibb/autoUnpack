e60dfaf9bb0ab309bd36241735782bee5c100c36
znibb <pontus.pson@gmail.com>
2016-11-04 20:55:44 +0100

    Now creates RAR_DIR folder if it doesn't already exist

--- replace_rars.sh
+++ replace_rars.sh
@@ -3,5 +3,10 @@
 ## Source directories
 . directories.txt
 
+## Create rar dir if does not already exists
+if [[ ! -d $RAR_DIR ]]; then
+  mkdir $RAR_DIR
+fi
+
 ## Copy example rars from templates folder to rar folder
 cp -u $TEMPLATE_DIR/*.rar $RAR_DIR
