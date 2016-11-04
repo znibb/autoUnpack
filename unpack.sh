f50bc7f700cd81cbb3f5cf66c94b07d7a42f20d1
znibb <pontus.pson@gmail.com>
2016-11-04 19:38:22 +0100

    Implemented non-categorizing unpacking

--- unpack.sh
+++ unpack.sh
@@ -1,3 +1,12 @@
-#!/bin/sh
-RAR_FOLDER=rar	# Probably a good idea to use absolute path instead
-UNPACK_FOLDER=unpacked
+#!/bin/bash
+. directories.txt
+
+for file in $RAR_DIR/*.rar; do
+  FILE_NAME=$(basename -s .rar $file)
+  TARGET="$UNPACK_DIR/$FILE_NAME"
+
+  if [[ ! -d "$TARGET" ]]; then
+    mkdir $TARGET
+    unrar x $file $TARGET > /dev/null
+  fi
+done
